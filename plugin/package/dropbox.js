c31cc7b12fda05fd00f364556276a542feb97c36
somang <sixjuwel@gmail.com>
2013-09-06 23:10:31 -0400

    dropbox browse2

--- plugin/package/dropbox.js
+++ plugin/package/dropbox.js
@@ -1,4 +1,8 @@
 
 function checkDropbox(){
-    	alert("I am an authenticated user!");
+    // add an event listener to a Chooser button
+    document.getElementById("db-chooser").addEventListener("DbxChooserSuccess",
+        function(e) {
+            alert("Here's the chosen file: " + e.files[0].link)
+        }, false);
 }
