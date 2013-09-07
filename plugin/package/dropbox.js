033fa3350e82c9ba022dd2f7086be0fbf288af2d
Yong Ro Park <yongro8912@gmail.com>
2013-09-07 17:35:42 -0400

    setKey implemented but don't know if it's working

--- plugin/package/dropbox.js
+++ plugin/package/dropbox.js
@@ -27,22 +27,46 @@ function getContent(theUrl){
 }
 
 function setKey(uid,publickey){
+
+  var friendList;
   var person = {"uid": uid, "public": publickey};
-
-  var new_friendlist = {'data':[]};
-  var prev_friendlist = localStorage.getItem("friendlist");
-
-  if (prev_friendlist == null){
-    // if prev freindlist is null, then we add a new array.
-  	new_friendlist.data.push(JSON.stringify(person));
-    localStorage.setItem("friendlist", JSON.stringify(new_friendlist));
-
-  }else {
-    // if there's list already, then we get the localstorage item and add to it, store the new list.
-    console.log(typeof prev_friendlist);
-    //new_friendlist = prev_friendlist.push(JSON.stringify(person));
-    
+  var uidExists = false;
+  
+  // Create object representing friendList in localStorage
+  if (localStorage.getItem("friendList") == null) {
+    friendList = [];
   }
-  //console.log(new_friendlist.data.length);
-  console.log(new_friendlist.data);
+  else {
+    friendList = JSON.parse(localStorage.getItem("friendList"));
+  }
+
+  // First if uid exists in localStorage
+  for (var i in friendList) {
+    if (friendList[i]["uid"] == uid) {
+      uidExists == true;
+      break;
+    }
+  }
+
+  if (! uidExists) {
+    friendList.push(person);
+  }
+
+  // Now synchronize
+  localStorage.setItem("friendList", JSON.stringify(friendList));
+
+
+  // if (prev_friendlist == null){
+  //   // if prev freindlist is null, then we add a new array.
+  // 	new_friendlist.data.push(JSON.stringify(person));
+  //   localStorage.setItem("friendlist", JSON.stringify(new_friendlist));
+
+  // }else {
+  //   // if there's list already, then we get the localstorage item and add to it, store the new list.
+  //   console.log(typeof prev_friendlist);
+  //   //new_friendlist = prev_friendlist.push(JSON.stringify(person));
+    
+  // }
+  // //console.log(new_friendlist.data.length);
+  // console.log(new_friendlist.data);
 }
