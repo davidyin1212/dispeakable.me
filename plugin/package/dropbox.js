8b5fa94c0f9fa20df7939db3b5eac4c1d04948be
somang <sixjuwel@gmail.com>
2013-09-06 23:41:40 -0400

    ver00

--- plugin/package/dropbox.js
+++
@@ -1,8 +0,0 @@
-function chooser(){
-  // add an event listener to a Chooser button
-  document.getElementById("db-chooser").addEventListener("DbxChooserSuccess",
-  	function(e) {
-  	  alert("Here's the chosen file: " + e.files[0].link)
-  	}, false);
-}
-
