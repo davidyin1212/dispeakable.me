a51ea8c1b702b1d7018a95f1d47cd8a116d79109
Yong Ro Park <yongro8912@gmail.com>
2013-09-07 13:39:01 -0400

    Selection, injection done

--- plugin/package/content_script.js
+++ plugin/package/content_script.js
@@ -1,11 +1,21 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
   	if (request.method == "getSelection"){
-      sendResponse({data: window.getSelection().toString()});
+  		// First, Inject new Hidden div into selected area
+  		var selection = window.getSelection();
+  		var newDOM = document.createElement("span");
+  		newDOM.setAttribute("class", "newArea");
+  		selection.getRangeAt(0).surroundContents(newDOM);
+  		//document.body.appendChild(newDOM);// DELETE THIS
+  		// Encryption algorithm here
+
+      sendResponse({data: selection.toString()});
      
+
   	}
     else {
       sendResponse({}); // Invalid method
   	}
 });
+
 
