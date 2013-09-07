dcb136599a227c4568185e82328e413e03e81851
somang <sixjuwel@gmail.com>
2013-09-06 23:30:23 -0400

    ver2

--- plugin/package/dropbox.js
+++ plugin/package/dropbox.js
@@ -1,3 +1,7 @@
-function checkDropbox(){
-	window.open()
-}
+// add an event listener to a Chooser button
+document.getElementById("db-chooser").addEventListener("DbxChooserSuccess",
+  function(e) {
+  	alert("Here's the chosen file: " + e.files[0].link)
+  }, false);
+
+
