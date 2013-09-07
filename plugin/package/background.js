b72c5f1a954a79aa3a5976ded6034b72782adbfc
Yong Ro Park <yongro8912@gmail.com>
2013-09-07 10:29:48 -0400

    getSelection done

--- plugin/package/background.js
+++ plugin/package/background.js
@@ -1,27 +1,28 @@
 // listening for an event / one-time requests
 // coming from the popup
-// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-//     switch(request.type) {
-//         case "getSelection":
-//             getSelection();
-//         break;
-//     }
-//     return true;
-// });
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    switch(request.type) {
+        case "getSelection":
+        	console.log("shit");
+          getSelection();
+        break;
+    }
+    return true;
+});
 
 
 
-// function getSelection() {
-// 	chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
-// 	    showSelected(response.data);
-// 	  });
-// }
+function getSelection() {
+	chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
+	    showSelected(response.data);
+	  });
+}
 
 
 
-// function showSelected(data) {
-// 	var popups = chrome.extension.getViews();
-// 	for (var i in popups) {
-// 		popups[i].document.write("FUCKOFF");
-// 	}
-// }
+function showSelected(data) {
+	var popups = chrome.extension.getViews();
+	for (var i in popups) {
+		popups[i].document.write("FUCKOFF");
+	}
+}
--- plugin/package/popup.js
+++ plugin/package/popup.js
@@ -1,24 +1,17 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('input').addEventListener('click', clickHandler);
+  document.getElementById("getSelectionBtn").addEventListener('click', getSelectionHandler);
 });
 
 
 // Send message to background script
-function clickHandler() {
+function getSelectionHandler() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {method: "getSelection"}, function(response){
+	  	console.log(JSON.stringify(response));
+	  });
+	});
 
 
-	// chrome.extension.sendMessage({
-	// 	type: "getSelection"
-	// });
-	alert("test");
+	
 }
-
-
-// $(document).ready(){
-// 	$('#button1').on("click", function(){
-// 	alert("shit");
-// 	});
-// };
-
-
