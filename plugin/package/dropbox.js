21fb4d321845c3935be77a62d438c0b0911b6bb6
somang <sixjuwel@gmail.com>
2013-09-06 22:49:44 -0400

    alert to check authentication

--- plugin/package/dropbox.js
+++ plugin/package/dropbox.js
@@ -1,12 +1,19 @@
 var client = new Dropbox.Client({key: "j2q6r6vvd5g8ulm"});
 
-// Try to finish OAuth authorization.
-client.authenticate({interactive: false}, function (error) {
+
+
+
+
+function checkDropbox(){
+	// Try to finish OAuth authorization.
+	client.authenticate({interactive: false}, function (error) {
     if (error) {
         alert('Authentication error: ' + error);
     }
-});
+	});
 
-if (client.isAuthenticated()) {
-    // Client is authenticated. Display UI.
+	if (client.isAuthenticated()) {
+    	// Client is authenticated. Display UI.
+    	alert("I am an authenticated user!");
+	}
 }
