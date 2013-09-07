d46feca210bd949c23273c9ac4e57e682502dd1d
somang <sixjuwel@gmail.com>
2013-09-06 23:00:38 -0400

    dropbox button
    choose from dropbox

--- plugin/package/dropbox.js
+++ plugin/package/dropbox.js
@@ -1,19 +1,4 @@
-var client = new Dropbox.Client({key: "j2q6r6vvd5g8ulm"});
-
-
-
-
 
 function checkDropbox(){
-	// Try to finish OAuth authorization.
-	client.authenticate({interactive: false}, function (error) {
-    if (error) {
-        alert('Authentication error: ' + error);
-    }
-	});
-
-	if (client.isAuthenticated()) {
-    	// Client is authenticated. Display UI.
     	alert("I am an authenticated user!");
-	}
 }
