71de8eda69e820b4a044c8f43ab2ac57ed265287
somang <sixjuwel@gmail.com>
2013-09-06 23:24:17 -0400

    testing for server, dropbox saver getter

--- plugin/package/dropbox.js
+++ plugin/package/dropbox.js
@@ -1,8 +1,3 @@
-
 function checkDropbox(){
-    // add an event listener to a Chooser button
-    document.getElementById("db-chooser").addEventListener("DbxChooserSuccess",
-        function(e) {
-            alert("Here's the chosen file: " + e.files[0].link)
-        }, false);
+	window.open()
 }
