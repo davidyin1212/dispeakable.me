5e50749716b0bbdd2a9131d5fc94871a656c2e65
KimY <davekim@ebmeducator.com>
2013-09-07 14:51:16 -0400

    merging

--- plugin/package/background.js
+++ plugin/package/background.js
@@ -1,15 +1,30 @@
-// // listening for an event / one-time requests
-// // coming from the popup
+// listening for an event / one-time requests
+// coming from the popup
 // chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-//     switch(request.type) {
-//         case "getSelection":
-//         	console.log("shit");
-//           getSelection();
-//         break;
-//     }
-//     return true;
+
+//   switch(request.type) {
+//     case "getSelection":
+//     	console.log("shit");
+//       getSelection();
+//       break;
+//     case "openLink":
+//       console.log("in");
+//       chrome.tabs.create({ url: "dispeakable.me" });
+//       break;
+//   }
+//   return true;
 // });
 
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  }
+);
 
 
 // function getSelection() {
@@ -19,10 +34,10 @@
 // }
 
 
-
 // function showSelected(data) {
 // 	var popups = chrome.extension.getViews();
 // 	for (var i in popups) {
 // 		popups[i].document.write("FUCKOFF");
 // 	}
 // }
+
--- plugin/package/popup.js
+++ plugin/package/popup.js
@@ -1,6 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("getSelectionBtn").addEventListener('click', getSelectionHandler);
+  document.getElementById('lead').addEventListener('click', openPage);
 });
 
 
@@ -11,7 +12,16 @@ function getSelectionHandler() {
 	  	console.log(JSON.stringify(response));
 	  });
 	});
-
-
-	
 }
+
+function openPage(){
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //   chrome.tabs.sendMessage(tabs[0].id, {method: "openLink"}, function(response){
+  //     console.log(JSON.stringify(response));
+  //   });
+  // });
+chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+  console.log(response.farewell);
+});
+}
+
