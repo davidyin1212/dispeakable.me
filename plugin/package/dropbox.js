b237c07a0ca6dc8d1ea01aa750e480b79a3c63c5
somang <sixjuwel@gmail.com>
2013-09-07 00:01:41 -0400

    manifest edit

--- plugin/package/dropbox.js
+++ plugin/package/dropbox.js
@@ -1,4 +1,10 @@
+/*
 document.getElementById("db-chooser").addEventListener("DbxChooserSuccess",
   function(e) {
   	alert("Here's the chosen file: " + e.files[0].link)
 }, false);
+*/
+
+function checkDropbox(){
+	
+}
