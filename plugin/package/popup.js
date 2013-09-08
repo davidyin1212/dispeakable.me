5e635e0d69da7dd78c2384c945298da3e4f4d56e
Wil Wong <wilzwong@gmail.com>
2013-09-08 05:47:21 -0400

    plugin ui done

--- plugin/package/popup.js
+++ plugin/package/popup.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("getSelectionBtn").addEventListener('click', getSelectionHandler);
 
   //document.getElementById('lead').addEventListener('click', openPage);
-  document.getElementById('decryptBtn').addEventListener('click', decryptPage);
+  //document.getElementById('decryptBtn').addEventListener('click', decryptPage);
 
   document.getElementById('aesGenBtn').addEventListener('click', genAES);
   document.getElementById('addFriend').addEventListener('click', addFriend);
@@ -170,28 +170,51 @@ var encrypt = function(uid, msg){
     chrome.tabs.insertCSS(null, {file: "inject.css"});
     // Handler for .ready() called.
     var $goggles = $('#goggles');
-    var mouseleft = false;
-    $(document).on('mouseup', function(e){
-      $goggles.css({
-        'top': 33,
-        'left': 29
-      });
-      if(e.pageX<0 || e.pageX>300 || e.pageY < 0 || e.pageY > 626){
-        //TODO send message to background
-        //chrome.tabs.executeScript(null, {file: "spy_goggles.js"});
-        $goggles.hide();
-        decryptPage();
-        // chrome.runtime.sendMessage({greeting: "potato"}, function(response) {        
-        //   if(response.farewell == "banana"){
-        //     $goggles.hide();
-        //   }else{
-        //     console.log(response.farewell);
-        //   }
-        // });
-      }
-      return false;
+    $goggles.click(function(){
+      decryptPage();
+      $goggles.fadeOut().delay( 800 ).fadeIn();
     });
-    $goggles.draggable();
+    $('li.decrypt').click(function(){
+      $('li').removeClass('active');
+      $(this).addClass('active');
+      $('section:not(.decrypt)').hide();
+      $('section.decrypt').show();
+    });
+    $('li.encrypt').click(function(){
+      $('li').removeClass('active');
+      $(this).addClass('active');
+      $('section:not(.encrypt)').hide();
+      $('section.encrypt').show();
+    });
+    $('li.options').click(function(){
+      $('li').removeClass('active');
+      $(this).addClass('active');
+      $('section:not(.options)').hide();
+      $('section.options').show();
+    });
+    //CODE FOR DRAGGABLE GoGGLES
+    // var mouseleft = false;
+    // $(document).on('mouseup', function(e){
+    //   $goggles.css({
+    //     'top': 33,
+    //     'left': 29
+    //   });
+    //   if(e.pageX<0 || e.pageX>300 || e.pageY < 0 || e.pageY > 626){
+    //     //TODO send message to background
+    //     //chrome.tabs.executeScript(null, {file: "spy_goggles.js"});
+    //     $goggles.hide();
+    //     decryptPage();
+    //     // chrome.runtime.sendMessage({greeting: "potato"}, function(response) {        
+    //     //   if(response.farewell == "banana"){
+    //     //     $goggles.hide();
+    //     //   }else{
+    //     //     console.log(response.farewell);
+    //     //   }
+    //     // });
+    //   }
+    //   return false;
+    // });
+    // $goggles.draggable();
   });
 // })(jQuery);
 
